732f12a094f9b86e1d7f21e79f0021923a75d6d8
土屋絵里佳 <>
2025-08-31 22:02:33 +0900

    追加

--- functions.php
+++ functions.php
@@ -1,18 +1,65 @@
 <?php
+// WordPressにCSSとJSファイルを読み込ませるための関数
+function my_theme_scripts() {
+    // テーマのCSSを読み込む
+    wp_enqueue_style( 'samurai-university-style', get_stylesheet_uri() );
+    wp_enqueue_style( 'samurai-university-style-main', get_template_directory_uri() . '/css/style.css' );
 
-// 登録済みのjQueryを解除して、登録し直す
-function remove_default_jquery()
-{
-    // 管理画面でないなら
-    if (!is_admin()) {
-        wp_deregister_script('jquery');
+    // BootstrapのCSSを読み込む
+    wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/plugins/bootstrap-4.7.0/bootstrap.min.css' );
 
-        wp_enqueue_script('jquery', get_template_directory_uri() . '/js/jquery-3.2.1.min.js', array(), 3.2, true);
-        wp_enqueue_script('popper_js', get_template_directory_uri() . '/styles/bootstrap4/popper.js', array(), 4.0, true);
-        wp_enqueue_script('bootstrap4_js', get_template_directory_uri() . '/styles/bootstrap4/bootstrap.min.js', array(), 4.0, true);
-    }
+    // Font AwesomeのCSSを読み込む
+    wp_enqueue_style( 'font-awesome-css', get_template_directory_uri() . '/plugins/font-awesome-4.7.0/css/font-awesome.min.css' );
+
+    // jQueryを読み込む
+    wp_enqueue_script( 'jquery', get_template_directory_uri() . '/js/jquery-3.2.1.min.js', array(), '3.2.1', true );
+
+    // Popper.jsを読み込む
+    wp_enqueue_script( 'popper-js', get_template_directory_uri() . '/js/popper.js', array('jquery'), '', true );
+
+    // BootstrapのJSを読み込む
+    wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/plugins/bootstrap-4.7.0/bootstrap.min.js', array('jquery', 'popper-js'), '', true );
+
+    // カスタムスクリプトを読み込む
+    wp_enqueue_script( 'custom-js', get_template_directory_uri() . '/js/script.js', array('jquery', 'popper-js', 'bootstrap-js'), '1.0.0', true );
 }
 
+// WordPressの読み込みフックに登録する
+add_action( 'wp_enqueue_scripts', 'my_theme_scripts' );
 
-add_action('wp_enqueue_scripts', 'remove_default_jquery');
-add_theme_support('post-thumbnails');
+// ここから下は、テーマを動かすための設定です
+if ( ! function_exists( 'samurai_university_setup' ) ) :
+	function samurai_university_setup() {
+		load_theme_textdomain( 'samurai-university', get_template_directory() . '/languages' );
+		add_theme_support( 'automatic-feed-links' );
+		add_theme_support( 'title-tag' );
+		add_theme_support( 'post-thumbnails' );
+		register_nav_menus(
+			array(
+				'menu-1' => esc_html__( 'Primary', 'samurai-university' ),
+			)
+		);
+		add_theme_support(
+			'html5',
+			array(
+				'search-form',
+				'comment-form',
+				'comment-list',
+				'gallery',
+				'caption',
+			)
+		);
+		add_theme_support( 'custom-background', apply_filters( 'samurai_university_custom_background_args', array(
+			'default-color' => 'ffffff',
+			'default-image' => '',
+		) ) );
+		add_theme_support( 'customize-selective-refresh-widgets' );
+		add_theme_support( 'custom-logo', array(
+			'height'      => 250,
+			'width'       => 250,
+			'flex-width'  => true,
+			'flex-height' => true,
+		) );
+	}
+endif;
+add_action( 'after_setup_theme', 'samurai_university_setup' );
--- header.php
+++ header.php
@@ -1,71 +1,59 @@
-  <!DOCTYPE html>
-  <head>
-    <title>samurai university</title>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="description" content="samurai university" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/styles/bootstrap4/bootstrap.min.css" />
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package samurai-university
+ */
+
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css" />
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/plugins/bootstrap-4.7.0/bootstrap.min.css" />
     <link href="<?php echo get_template_directory_uri(); ?>/plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/styles/main_styles.css" />
     <?php wp_head(); ?>
-  </head>
+</head>
 
-  <body>
-    <div class="super_container">
-      <header class="header">
-        <div class="header_container">
-          <div class="">
-            <nav class="navbar navbar-expand-lg">
-              <div class="logo_container">
-                <div class="logo_text">
-                  <a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/logo_big.png" />
-                    <span>Samurai University</span>
-                  </a>
-                </div>
-              </div>
-              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <i class="fa fa-bars toggle-menu" aria-hidden="true"></i>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav flex-row ml-md-auto d-md-flex main_nav">
-  <li class="nav-item">
-<a class="nav-link" href="<?php echo home_url(); ?>/category/news">
-  NEWS
-  <p>ニュース</p>	
-  </a>	
-  </li>	
-  <li class="nav-item">	
-  <a class="nav-link" href="<?php echo home_url(); ?>/category/event">
-  EVENT	
-  <p>イベント</p>	
-  </a>	
-  </li>	
-  <li class="nav-item"> 
-  <a class="nav-link" href="<?php echo home_url(); ?>/category/graduates">
-  GRADUATES
-  <p>卒業生の声</p>  
-  </a>  
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'samurai-university' ); ?></a>
 
-  </li>	
-  <li class="nav-item">	
-    <a class="nav-link" href="<?php echo home_url(); ?>/courses">
-  COURSES	
-  <p>コース</p>	
-  </a>	
-  </li>	
-  <li class="nav-item">	
-  <a class="nav-link" href="<?php echo home_url(); ?>/about_us">
-  ABOUT US	
-  <p>侍大学について</p>	
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </nav>
-          </div>
-        </div>
-      </header>
-    
+	<header id="masthead" class="site-header">
+		<div class="site-branding">
+			<?php
+			if ( is_front_page() && is_home() ) :
+				?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php
+			else :
+				?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php
+			endif;
+			$samurai_university_description = get_bloginfo( 'description', 'display' );
+			if ( $samurai_university_description || is_customize_preview() ) :
+				?>
+				<p class="site-description"><?php echo $samurai_university_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+			<?php endif; ?>
+		</div><!-- .site-branding -->
+
+		<nav id="site-navigation" class="main-navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'samurai-university' ); ?></button>
+			<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'primary-menu',
+				)
+			);
+			?>
+		</nav><!-- #site-navigation -->
+	</header><!-- #masthead -->
